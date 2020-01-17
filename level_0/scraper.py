e1aac9bfc686860b00760a01364ec3c113dd80e8
danrocus1994 <danrodcastillo1994@gmail.com>
2020-01-17 15:24:22 -0500

    level_1

--- level_0/scraper.py
+++ level_0/scraper.py
@@ -3,9 +3,9 @@ import mechanize
 
 br = mechanize.Browser()
 br.open("http://158.69.76.135/level0.php")
-for i in range(4):
+for i in range(1024):
     br.select_form(nr=0)
     input = br.form.find_control("id")
     input.value = '1305'
     res = br.submit()
-    print(i, "vote")
+    print("Vote", i + 1)
