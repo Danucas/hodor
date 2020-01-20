b284b3591e4ada66ad6270575a0bf7ae2df3f430
danrocus1994 <danrodcastillo1994@gmail.com>
2020-01-20 11:03:56 -0500

    level 5

--- level_5/test.py
+++ level_5/test.py
@@ -5,20 +5,39 @@ import cv2
 import os
 import argparse
 import numpy as np
+from subprocess import check_output
 
 def cond_pic():
-    image = cv2.imread('captcha.png')
+    path = "captcha.png"
+    image = cv2.imread(path)
+    scala = 80
+    w = int(image.shape[1] * scala / 100)
+    h = int(image.shape[0] * scala / 100)
+    dim = (w, h)
+
+    image = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
     hsv_img = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    cv2.imwrite("cond_cap.png", hsv_img)
-    return "cond_cap.png"
+    #cv2.imwrite("hsv_img.png", hsv_img)
+    #return "cond_cap.png"
 
 
-    #low = np.array([0, 0, 0], dtype = 'uint16')
-    #high = np.array([70, 70, 70], dtype = 'uint16')
-    #mask = cv2.inRange(image, low, high)
+    low = (0, 0, 128)
+    high = (10, 10, 132)
+    mask1 = cv2.inRange(hsv_img, low, high)
+    low = (0, 0, 0)
+    high = (124, 124, 124)
+    mask2 = cv2.inRange(hsv_img, low, high)
 
-    #image = cv2.bitwise_and(image, image, mask=mask)
+    mask = cv2.bitwise_xor(mask1, mask2)
+    target = cv2.bitwise_and(hsv_img, hsv_img, mask=mask)
 
+    target = cv2.bitwise_not(target, target, mask=mask)
+
+    cv2.imwrite("cond_cap_mask.png", mask)
+    #cv2.imwrite("cond_cap.png", target)
+    path = "cond_cap_mask.png"
+    return pytesseract.image_to_string(Image.open(path))
     #cv2.imwrite("mask.png", mask)
     #cv2.imwrite("gray.png", image)
+
     
