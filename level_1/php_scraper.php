34d8c48f19e4e281207559d0c45f0f142421ccc2
danrocus1994 <danrodcastillo1994@gmail.com>
2020-01-21 20:14:50 -0500

    dsa

--- level_1/php_scraper.php
+++ level_1/php_scraper.php
@@ -4,55 +4,83 @@ function curl($url){
      /**
      *Create a conexion, scrapes the key and POST the form
      */
+
      $ch = curl_init($url);
      curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
      curl_setopt($ch, CURLOPT_HEADER, 1);
 
      /*Getting and printing the html source*/
+
      $source = curl_exec($ch);
      /*echo $source, "\n";*/
 
-     /*Getting cookie is the same as key*/
+     /*Getting cookie HoldTheDoor from the header this will be the key also*/
+
      preg_match_all('/^Set-Cookie:\s*([^;]*)/mi', $source, $matches);
      $cookies = array();
      foreach($matches[1] as $item){
-     parse_str($item, $cookie);
-     $cookies = array_merge($cookies, $cookie);
-			 }
-     echo "cookies:\n";
+         parse_str($item, $cookie);
+         $cookies = array_merge($cookies, $cookie);
+     }
+     echo "cookies:\nHold The Door:\t";
      $f_key = $cookies['HoldTheDoor'];
+     echo $f_key, "\n";
+
      /*Building the POST request*/
+
      $query = "id=1305&holdthedoor=Submit&key=";
      $query .= $f_key;
+
+     /*Configurate the curl options*/
 
      curl_setopt($ch, CURLOPT_HTTPHEADER, array("Cookie: HoldTheDoor=".$f_key));
      curl_setopt($ch, CURLOPT_URL, $url);
      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
+     /*Set the culr mode to POST*/
+
      curl_setopt($ch, CURLOPT_POST, 1);
      curl_setopt($ch, CURLOPT_POSTFIELDS, $query);
-     $response = trim(curl_exec($ch));
+
+     /*Making the request*/
+
+     $response = curl_exec($ch);
+
+     /*This closes the session and conexion*/
+
      curl_close($ch);
+
      return $response;
-     /*echo $query, "\n";*/
 }
 
 function vote(){
+     /**
+     * This checks the response after sending the POST request
+     */
 
      $res = curl("http://158.69.76.135/level1.php");
-     if ($res == "See you later hacker! [5]"){
+     if ($res == "See you later hacker! [5]" || $res == "See you later hacker! [4]"){
      echo $res, "\n";
      } else {
-     	   echo "\033[32mSuccess\033[0m", "\n";
+       	   /* for printing the actual count*/
+	   $st = "<tr>\n    <td>\n1305    </td>\n    <td>\n";
+	   $counter_array = array();
+	   $ini = stripos($res, $st) + strlen($st);
+	   for ($i = $ini; $res[$i] != " "; $i++){
+	       array_push($counter_array, $res[$i]);
+	   }
+	   $counter = join($counter_array);
+     	   echo "\033[32m\nSuccess\033[0m\n", "\n";
+	   return $counter;
      }
 
 }
 
-for ($i = 0; $i < 4096; $i++)
+for ($i = 0; $i < 1000; $i++)
 {
-	vote();
-	echo "vote #", $i + 1, "\n";
+	$count = vote();
+	echo "vote #", $count, "\n";
 }
 
 
